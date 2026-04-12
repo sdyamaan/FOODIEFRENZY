05fc1c3200b3415d824b60382d821c47f27e0f9e
sdyamaan <amansayyed.1305@gmail.com>
2026-04-12 15:23:08 +0530

    complete the project

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -117,7 +117,7 @@ const Footer = () => {
               rel="noopener noreferrer"
               className="text-lg font-sacramento bg-gradient-to-r from-amber-400 via-amber-500 to-amber-400 bg-clip-text text-transparent hover:text-purple-300 transition-all duration-500"
             >
-              Designed by Hexagon Digital Services
+              Designed by Aman Sayyed & Soham Shelar
             </a>
           </div>
         </div>
